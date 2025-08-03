8045ba031d34687ff77c19c93a45f54e5b20339d
Lucas-Bispo <lukaobispo@gmail.com>
2025-08-03 09:53:30 -0300

    Teste 6 Login com senha remota segura

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_N7k7lai51', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7gklgi9vrrjnc86kafnq1u2ncv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6qpn0h5nr7ol7qhq904e309un9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
