0978d3e192bc696803f28d6ed7f92a87fbc2f123
Lucas-Bispo <lukaobispo@gmail.com>
2025-08-03 09:01:37 -0300

    Teste 5, alteracao no ALLOW_USER_AUTH

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_N7k7lai51', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4ht4g6fd5mjqd9qtt9bh9fo4l0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7gklgi9vrrjnc86kafnq1u2ncv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
