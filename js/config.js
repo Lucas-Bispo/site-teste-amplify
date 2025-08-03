b7f03e4348fa196d251ea28d6f880892ad297c92
Lucas-Bispo <lukaobispo@gmail.com>
2025-08-03 10:22:28 -0300

    teste 7 SAP

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_N7k7lai51', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6qpn0h5nr7ol7qhq904e309un9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '66j5fdb3ahuoj5qltcsdg37u8i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
